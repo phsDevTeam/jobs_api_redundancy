27c3c77cdaad353634dc606ffd67faf98e81ed34
Henry Ladia <henry.ladia@pioneer-healthcare.com>
2023-12-27 11:12:35 +0800

    redundancy backend server with XML handler

---
+++ server.js
@@ -0,0 +1,28 @@
+// Import modules
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors'); // Import the cors middleware
+
+// Create express app
+const app = express();
+const port = 3000;
+
+// Use cors middleware to enable CORS
+app.use(cors());
+
+// Retrieve XML data
+app.get('/getXmlData', async (req, res) => {
+  try {
+    const response = await fetch('https://pioneerhealthcare.my.salesforce-sites.com/apex/XMLfeedPageLinkedin');
+    const data = await response.text();
+    res.header('Content-Type', 'application/xml'); // Set the Content-Type header to indicate XML
+    res.send(data);
+  } catch (error) {
+    console.error('Error fetching XML data:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
